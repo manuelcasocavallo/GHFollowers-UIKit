2833cf55ca23fdacfb6150662e2ca4b7e71e8f6c
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-05-30 18:25:04 +0200

    Updated network manager, now using Result type instead of optionals

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class FollowersListVC: UIViewController {
-
+    
     var username: String!
     
     override func viewDidLoad() {
@@ -16,20 +16,23 @@ class FollowersListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: errorMessage!.rawValue, buttonTitle: "Ok")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            
+            case .success(let followers):
+                print(followers)
+            
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad stuff happened", message: error.rawValue, buttonTitle: "Ok")
+                
             }
-            print("Followers.count = \(followers.count)")
-            print(followers)
         }
-
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
-
+    
 }
