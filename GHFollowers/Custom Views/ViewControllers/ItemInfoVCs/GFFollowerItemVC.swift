508114fd3d5ae5b815ce445b9eb818a6b9f41b29
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-06-10 14:51:21 +0200

    Created Follower and Repo VCs for the UserInfo modal screen; updated Constants file

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,24 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by Manuel Casocavallo on 10/06/21.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+    
+}
--- GHFollowers/Utilities/Constants.swift
+++ GHFollowers/Utilities/Constants.swift
@@ -8,5 +8,9 @@
 import Foundation
 
 enum SFSymbols {
-    static let location = "mappin.and.ellipse"
+    static let location     = "mappin.and.ellipse"
+    static let repos        = "folder"
+    static let gists        = "text.alignleft"
+    static let following    = "person.2"
+    static let followers    = "heart"
 }
