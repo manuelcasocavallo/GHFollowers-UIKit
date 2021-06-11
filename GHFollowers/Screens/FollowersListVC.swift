b7d392103b6854632b935095367bc07f7f24f1fb
Manuel Casocavallo <manuelcasocavallo@gmail.com>
2021-06-11 14:34:04 +0200

    Project completed:  v1.0

--- GHFollowers/Screens/FollowersListVC.swift
+++ GHFollowers/Screens/FollowersListVC.swift
@@ -44,6 +44,8 @@ class FollowersListVC: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        let favoriteButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(favoriteButtonTapped))
+        navigationItem.rightBarButtonItem = favoriteButton
     }
     
     
@@ -106,8 +108,32 @@ class FollowersListVC: UIViewController {
         snapshot.appendItems(followers)
         DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
     }
+    
+    
+    @objc func favoriteButtonTapped() {
+        showLoadingView()
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            self.dismissLoadingView()
+            
+            switch result {
+            case .success(let user):
+                let favorite = Follower(login: user.login, avatarUrl: user.avatarUrl)
+                PersistenceManager.updateWith(favorite: favorite, actionType: .add) { [weak self] error in
+                    guard let self = self else { return }
+                    guard let error = error else {
+                        self.presentGFAlertOnMainThread(title: "Success", message: "You have successfully saved this user in your favorites.", buttonTitle: "Ok")
+                        return
+                    }
+                    self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+                }
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something went wrong", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
+    }
 }
-
 
 
 
@@ -138,7 +164,6 @@ extension FollowersListVC: UICollectionViewDelegate {
         
         present(navController, animated: true)
     }
-    
 }
 
 
--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -8,10 +8,14 @@
 import Foundation
 
 enum GFError: String, Error {
-    case invalidUsername    = "This username created an invalid request.\nPlease try again."
-    case unableToComplete   = "Unable to complete your request. Please check your internet connection."
-    case invalidResponse    = "Invalid response from the server.\nPlease try again."
-    case invalidData        = "The data received from the server was invalid.\nPlease try again."
-    case invalidURL         = "The url attached to this user is invalid."
-    case noFollowers        = "This user has no followers. \nWhat a shame! 🙁"
+    case invalidUsername        = "This username created an invalid request.\nPlease try again."
+    case unableToComplete       = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse        = "Invalid response from the server.\nPlease try again."
+    case invalidData            = "The data received from the server was invalid.\nPlease try again."
+    case invalidURL             = "The url attached to this user is invalid."
+    case noFollowers            = "This user has no followers. \nWhat a shame! 🙁"
+    case unableToLoadFavorites  = "An error occured while loading your favorites. \nPlease try again."
+    case unableToSaveFavorites  = "An error occured while saving your favorites. \nPlease try again."
+    case alreadyInFavorites     = "This user is already one of your favorites."
+
 }
